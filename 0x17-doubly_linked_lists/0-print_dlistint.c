16250f80f29cdd621fa20496deb82330943d9ef9
magoudi <youssefmagedan@gmail.com>
2024-03-21 07:38:29 -0700

    f

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t size = 0;
 	dlistint_t *current = h;
 
-	printf("%d\n", h->n);
+	//printf("%d\n", h->n);
 	while (current)
 	{
 		printf("%d\n", current->n);
