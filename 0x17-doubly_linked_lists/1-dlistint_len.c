ad238ebaad6ec3550ecd3bda7f659936238df812
magoudi <youssefmagedan@gmail.com>
2024-03-21 08:08:09 -0700

    f

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,18 +10,18 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	if (h == NULL)
 		return (0);
-	else
-	{
-		size_t size = 0;
-		
-		while (h->prev != NULL)
-		h = h->prev;
 
-		while (h != NULL)
-		{
-			h = h->next;
-			size++;
-		}
-		return (size);
+	size_t size = 0;
+
+	while (h->prev != NULL)
+	h = h->prev;
+
+	while (h != NULL)
+	{
+		size++;
+		h = h->next;
 	}
+
+	return (size);
+
 }
