64682d4af62794c6a085f6f59119dc7c28558d36
magoudi <youssefmagedan@gmail.com>
2024-04-12 11:28:47 -0700

    f

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	int index = key_index(key, ht->size);
+
+	current = ht->array[index];
+	while (current)
+	{
+		if(!strcmp(current->key, key))
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
