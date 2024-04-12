bb764da86d05a1c310eacbf63dc9ae06c065209c
magoudi <youssefmagedan@gmail.com>
2024-04-12 08:31:17 -0700

    f

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - jagbbfvhbfhsbbfh
- * @size: vcnjfgbjncjkrd
- * Return: sdknfjnvjfdnsfmcxk
+ * hash_table_create - creates a hash table
+ * @size: size of the hash table
+ *
+ * Return: pointer to hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *temp;
+	hash_table_t *table;
 
 	if (size == 0)
 		return (NULL);
 
-	temp = malloc(sizeof(hash_table_t));
-	if (temp == NULL)
+	table = calloc(1, sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
 
-	temp->array = malloc(size * sizeof(hash_node_t *));
-	temp->size = size;
-	if (temp->array == NULL)
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
 	{
-		free(temp);
+		free(table);
 		return (NULL);
 	}
-
-	return (temp);
+	return (table);
 }
