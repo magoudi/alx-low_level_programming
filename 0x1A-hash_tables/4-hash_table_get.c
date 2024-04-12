4a925d766e27f8ecdf7dca112bc26bbbdfaec4fb
magoudi <youssefmagedan@gmail.com>
2024-04-12 11:38:29 -0700

    f

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
-	
+
 	if (!(ht->array[index]))
 		return (NULL);
 
