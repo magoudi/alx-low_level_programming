87848b1bcb2178990477d0fc1750d7898de776df
magoudi <youssefmagedan@gmail.com>
2024-04-12 11:35:09 -0700

    f

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,7 +3,12 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *current;
-	int index = key_index((unsigned char *)key, ht->size);
+	int index;
+
+	if (!key)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
 
 	current = ht->array[index];
 	while (current)
