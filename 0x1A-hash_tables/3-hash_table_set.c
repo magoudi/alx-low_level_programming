f29a44b8d12c7c26dd0f7c5fac41f30d7df1265e
magoudi <youssefmagedan@gmail.com>
2024-04-12 11:12:32 -0700

    f

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (strlen(key) == 0 || !key || !value || !ht)
 		return (0);
-	if (!newnode || !valcpy || !keycpy || || !*key)
+	if (!newnode || !valcpy || !keycpy || !*key)
 		return (0);
 
 	index = key_index((unsigned char *)keycpy, ht->size);
