283941cbad65e08505ab104061726ad815fae7e3
magoudi <youssefmagedan@gmail.com>
2024-04-12 10:38:48 -0700

    f

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - jfbvabsfvkbsndkjfnc
+ * @ht: fbvhfbvshbgshrbf
+ * @key: jfsbvhbsfvhfbvshkd
+ * @value: fbvabdfvhbsfhgb
+ * Return: fjvbfvhsbfvbnskrj
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *newnode = malloc(sizeof(hash_node_t));
+	char *valcpy = strdup(value);
+	char *keycpy = strdup(key);
+	int index;
+
+	if (strlen(key) == 0)
+		return 0;
+
+	index = key_index((unsigned char *)keycpy, ht->size);
+	
+	newnode->key = keycpy;
+	newnode->value = valcpy;
+
+	if (ht->array[index] == NULL)
+	{
+		newnode->next = NULL;
+		(ht->array[index]) = newnode;
+	}
+	else
+	{
+		newnode->next = ht->array[index];
+		(ht->array[index]) = newnode;
+	}
+	return 1;
+}
