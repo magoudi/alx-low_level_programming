9f2e0314116c039c5a4822989952d5f8c4d82941
magoudi <youssefmagedan@gmail.com>
2024-04-12 08:10:41 -0700

    f

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *temp = malloc(sizeof(hash_table_t));
+	hash_table_t *temp;
 
-	if (size <= 0)
+	if (size == 0)
+		return (NULL);
+
+	temp = malloc(sizeof(hash_table_t));
+	if (temp == NULL)
 		return (NULL);
 
 	temp->array = malloc(size * sizeof(hash_node_t *));
 	temp->size = size;
+	if (temp->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
 	return (temp);
 }
