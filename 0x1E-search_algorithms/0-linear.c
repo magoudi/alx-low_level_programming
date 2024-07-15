1bc059b0cfbaba89492d0d7e31f2ed23f98f310d
magoudi <youssefmagedan@gmail.com>
2024-07-15 17:14:10 +0300

    f

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include <search_algos.h>
+
+/**
+ * linear_search - aerkgferjgoeirjg
+ * @array: sfgmksamgfsgf
+ * @size: rkgmasngmsc
+ * @value: jgnjasnrgonsrg
+ * Return: kdgnfndgjngjnjsgnvc
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < count; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
