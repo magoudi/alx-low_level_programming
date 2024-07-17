52bb4b8b0d4be6f7691a66cbee2e534e421380f7
magoudi <youssefmagedan@gmail.com>
2024-07-17 17:25:21 +0300

    f

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
  * binary_search - jdsngjanergn wrnf
@@ -10,5 +11,41 @@
 
 int binary_search(int *array, size_t size, int value)
 {
+	size_t i;
+	int ret;
 
+	if (array == NULL)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = 0; i < size - 1; i++)
+	{
+		printf("%d, ", array[i]);
+	}
+	printf("%d\n", array[size - 1]);
+
+	if (size == 1)
+	{
+		if (array[0] == value)
+			return (1);
+		else
+			return (-1);
+	}
+
+	if (array[(size - 1) / 2] == value)
+		return ((size - 1) / 2);
+	else if (array[(size - 1) / 2] < value)
+	{
+		ret = binary_search(array + (int)ceil((double)size / 2),
+							ceil((double)(size - 1) / 2), value);
+		if (ret == -1)
+			return (-1);
+		else
+			return (ret + ceil((double)(size - 1) / 2));
+	}
+	else
+	{
+		ret = binary_search(array, ((size - 1) / 2), value);
+		return (ret);
+	}
 }
